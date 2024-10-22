d8a01972bc6ef3c693a40803b6fbfe9a1040750a
Amr Shams <102551102+AmrShams9@users.noreply.github.com>
2024-10-23 02:02:41 +0300

    Initial Commit

---
+++ Tracking_System/src/app/user-registration/user-registration.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-registration',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './user-registration.component.html',
+  styleUrls: ['./user-registration.component.css']
+})
+export class UserRegistrationComponent {
+  registrationForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log(this.registrationForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
